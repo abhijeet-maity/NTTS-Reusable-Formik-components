976005652ca498b3befa09e935c632868c170591
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-03-03 17:36:14 +0530

    added textarea, select tag, radio

--- src/components/FormikContainer.jsx
+++ src/components/FormikContainer.jsx
@@ -1,13 +1,41 @@
 import React from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import "../App.css";
+import FormikControl from "./FormikControl";
 
 const FormikContainer = () => {
-  const initialValues = {};
-  const validationSchema = Yup.object({});
+  const dropdownOptions = [
+    { key: "Select a city", value: "" },
+    { key: "Kolkata", value: "Kolkata" },
+    { key: "Mumbai", value: "Mumbai" },
+    { key: "Manali", value: "Manali" },
+  ];
+
+  const radioOptions = [
+    {key: "male", value: "Male"},
+    {key: "female", value: "Female"},
+    {key: "others", value: "Others"},
+  ]
+
+  const initialValues = {
+    email: "",
+    description: "",
+    selectOption: "",
+    radioOption: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Required"),
+    description: Yup.string().required("Required"),
+    selectOption: Yup.string().required("Required"),
+    // radioOption : Yup.string().required("Required"),
+  });
+
   const onSubmit = (values) => {
     console.log("Form values", values);
   };
+  
   return (
     <Formik
       initialValues={initialValues}
@@ -15,9 +43,34 @@ const FormikContainer = () => {
       onSubmit={onSubmit}
     >
       {(formik) => {
-        <Form>
-            <submit type='submit'>Submit</submit>
-        </Form>;
+        return (
+          <Form>
+            <FormikControl
+              control="input"
+              type="email"
+              label="Email"
+              name="email"
+            />
+            <FormikControl
+              control="textarea"
+              label="Description"
+              name="description"
+            />
+            <FormikControl
+              control="select"
+              label="Select a city"
+              name="selectOption"
+              options={dropdownOptions}
+            />
+            <FormikControl 
+              control="radio"
+              label="Select Gender"
+              name="radioOption"
+              options={radioOptions}
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        );
       }}
     </Formik>
   );
