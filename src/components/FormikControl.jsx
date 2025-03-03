f403b60812722cd913a98c6154dfe56e80f84946
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-03-03 17:36:46 +0530

    changes for textarea, select, radio

---
+++ src/components/FormikControl.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import "../App.css";
+import Input from './Input';
+import Textarea from './Textarea';
+import Select from './Select';
+import RadioButton from './RadioButton';
+
+
+function FormikControl(props) {
+
+  const {control, ...rest} = props;
+  switch(control){
+    case 'input': {
+        return (
+            <Input {...rest}/>
+        )
+    }
+    case 'textarea': {
+        return (
+            <Textarea  {...rest}/>
+        )
+    }
+    case 'select': {
+        return (
+            <Select {...rest}/>
+        )
+    }
+    case 'radio': {
+        return (
+            <RadioButton {...rest}/>
+        )
+    }
+    case 'checkbox':
+    case 'date':
+    default: return null 
+  }
+}
+
+export default FormikControl
